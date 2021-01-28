305cafb7ad62573b215d1a63ed51c761027b09da
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-28 11:42:19 -0600

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -20,5 +20,6 @@ describe("Quotes app", () => {
     cy.get('input[name="author"]').should("exist");
     cy.get("#submitBtn").should("exist");
     cy.get('button[id="cancelBtn"]').should("exist");
+    cy.contains("Submit Quote");
   });
 });
